00812217f8689bc8c8b103f49cdf15b32a5fb39e
Iuliana <fiiuliana@yahoo.ca>
2020-07-04 00:00:39 +0300

    All the values in the DB are visible in the app /api/Products or /api/products/id

---
+++ StoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDBContext _context;
+        public ProductsController(StoreDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+           //  return "this is a list of products";
+           //not ok in therms of scalability
+            var products = await _context.Products.ToListAsync(); // instead use an async methond
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            //return "this is only one product";
+            return await _context.Products.FindAsync(id);
+        }
+    }
+
+}
