e28c8fd6f0e0bb0d983f3a0b59c2be667b4a2b18
Iuliana <fiiuliana@yahoo.ca>
2020-07-03 23:09:09 +0300

    Updating Windows Power Shell to 5.1 on Windows 7. Solving the Script Halted Error. DB existent in PC user

--- StoreAPI/Startup.cs
+++ StoreAPI/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using StoreAPI.Models;
 
 namespace StoreAPI
@@ -28,7 +21,11 @@ namespace StoreAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<StoreDBContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("StoreDBConnexionString")));
+            services.AddDbContext<StoreDBContext>(opt => 
+                opt.UseSqlServer(Configuration.GetConnectionString("StoreDBConnexionString")));
+
+        //    services.AddDbContext<StoreDBContext>(x =>
+        //        x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
